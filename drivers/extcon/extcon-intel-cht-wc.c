aae2a98bf80bae33b73d2db7ee4e54de4ae0f0c1
TranceLove <airwave209gt@gmail.com>
2017-04-08 16:16:25 +0800

    extcon: intel-cht-wc: Add usb_id sysfs attribute
    Some boards do not have the usb-id pin of the Whiskey Cove PMIC
    hooked up to the connector, leaving the connector permanently in
    gadget mode.
    
    This commit adds a usb_id sys attribute which user can write to to
    overwrite the usb-id pin value detected by the hardware so that
    they can use their OTG connector in host or ACA mode despite the
    usb-id pin not being connected.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/extcon/extcon-intel-cht-wc.c
+++ drivers/extcon/extcon-intel-cht-wc.c
@@ -76,6 +76,10 @@ enum cht_wc_usb_id {
 	USB_RID_C,
 };
 
+/* Strings matching the cht_wc_usb_id enum labels */
+static const char * const usb_id_str[] = {
+	"otg", "gnd", "float", "rid_a", "rid_b", "rid_c" };
+
 enum cht_wc_mux_select {
 	MUX_SEL_PMIC = 0,
 	MUX_SEL_SOC,
@@ -97,10 +101,14 @@ struct cht_wc_extcon_data {
 	struct extcon_dev *edev;
 	unsigned int previous_cable;
 	bool usb_host;
+	int usb_id;
 };
 
 static int cht_wc_extcon_get_id(struct cht_wc_extcon_data *ext, int pwrsrc_sts)
 {
+	if (ext->usb_id)
+		return ext->usb_id;
+
 	if (pwrsrc_sts & CHT_WC_PWRSRC_ID_GND)
 		return USB_ID_GND;
 	if (pwrsrc_sts & CHT_WC_PWRSRC_ID_FLOAT)
@@ -271,6 +279,35 @@ static irqreturn_t cht_wc_extcon_isr(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
+/* usb_id sysfs attribute for debug / testing purposes */
+static ssize_t usb_id_show(struct device *dev, struct device_attribute *attr,
+			   char *buf)
+{
+	struct cht_wc_extcon_data *ext = dev_get_drvdata(dev);
+
+	return sprintf(buf, "%s\n", usb_id_str[ext->usb_id]);
+}
+
+static ssize_t usb_id_store(struct device *dev, struct device_attribute *attr,
+			    const char *buf, size_t n)
+{
+	struct cht_wc_extcon_data *ext = dev_get_drvdata(dev);
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(usb_id_str); i++) {
+		if (sysfs_streq(buf, usb_id_str[i])) {
+			dev_info(ext->dev, "New usb_id %s\n", usb_id_str[i]);
+			ext->usb_id = i;
+			cht_wc_extcon_pwrsrc_event(ext);
+			return n;
+		}
+	}
+
+	return -EINVAL;
+}
+
+static DEVICE_ATTR(usb_id, 0644, usb_id_show, usb_id_store);
+
 static int cht_wc_extcon_sw_control(struct cht_wc_extcon_data *ext, bool enable)
 {
 	int ret, mask, val;
@@ -357,6 +394,7 @@ static int cht_wc_extcon_probe(struct platform_device *pdev)
 	}
 
 	platform_set_drvdata(pdev, ext);
+	device_create_file(ext->dev, &dev_attr_usb_id);
 
 	return 0;
 
@@ -369,6 +407,7 @@ static int cht_wc_extcon_remove(struct platform_device *pdev)
 {
 	struct cht_wc_extcon_data *ext = platform_get_drvdata(pdev);
 
+	device_remove_file(ext->dev, &dev_attr_usb_id);
 	cht_wc_extcon_sw_control(ext, false);
 
 	return 0;
